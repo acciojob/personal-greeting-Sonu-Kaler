c168d82c1a92ed31c92345dd7d884ec4d523efa5
Sonu-Kaler <kalers19@gmail.com>
2024-01-29 18:32:59 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+// import { set } from "cypress/types/lodash";
 
 const App = () => {
+  const [name,setName] = useState("");
+  function fun(e){
+    setName(e.target.value);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Enter your name:</p>
+        <input type="text" onChange={fun}/>
+        <p>{name}</p>
     </div>
   )
 }
